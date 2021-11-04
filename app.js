d0e1d25aec6fae8274334867dc7cdbdc1efc8caf
sumantharrabelli <arrabellisumanth85@gmail.com>
2021-11-03 20:33:41 -0500

    updated public/routes

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var gucciRouter = require('./routes/gucci');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/gucci', gucciRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
